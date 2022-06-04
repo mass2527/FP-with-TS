9de08b993ab596a6ac401bf1ec6d345066240a9c
mass2527 <nzp01@naver.com>
2022-06-04 13:55:47 +0900

    map, filter, each 함수 iterable 모든 iterable 객체에서 사용가능하도록 변경

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,10 @@ const users = [
   { id: 8, name: "MP", age: 23 },
 ];
 
-function filter<T>(iterable: T[], predicator: (value: T) => unknown): T[] {
+function filter<T>(
+  iterable: Iterable<T>,
+  predicator: (value: T) => unknown
+): T[] {
   const result: T[] = [];
   each(iterable, (value) => {
     if (predicator(value)) {
@@ -19,7 +22,7 @@ function filter<T>(iterable: T[], predicator: (value: T) => unknown): T[] {
   return result;
 }
 
-function map<T, U>(iterable: T[], modifier: (value: T) => U): U[] {
+function map<T, U>(iterable: Iterable<T>, modifier: (value: T) => U): U[] {
   const result: U[] = [];
   each(iterable, (value) => {
     result.push(modifier(value));
@@ -29,23 +32,25 @@ function map<T, U>(iterable: T[], modifier: (value: T) => U): U[] {
 
 // a. 30세 이상인 users를 거른다.
 // b. 30세 이상인 users의 names를 수집한다.
-console.log(
-  map(
-    filter(users, (user) => user.age >= 30),
-    (user) => user.name
-  )
-);
+// console.log(
+//   map(
+//     filter(users, (user) => user.age >= 30),
+//     (user) => user.name
+//   )
+// );
 // c. 30세 미만인 users를 거른다.
 // d. 30세 미만인 users의 ages를 수집한다.
-console.log(
-  map(
-    filter(users, (user) => user.age < 30),
-    (user) => user.age
-  )
-);
+// console.log(
+//   map(
+//     filter(users, (user) => user.age < 30),
+//     (user) => user.age
+//   )
+// );
 
-function each<T>(iterable: T[], iteratee: (value: T) => unknown) {
-  for (let i = 0; i < iterable.length; i++) {
-    iteratee(iterable[i]);
+function each<T>(iterable: Iterable<T>, iteratee: (value: T) => unknown) {
+  for (const value of iterable) {
+    iteratee(value);
   }
 }
+
+console.log(map(document.querySelectorAll("*"), (node) => node.nodeName));
